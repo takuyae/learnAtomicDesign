c34e164c04daf4d54a793ebe533901df27693c7c
takuyae <120998910+takuyae@users.noreply.github.com>
2023-01-25 13:08:58 +0900

    Initial commit

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img src="https://source.unsplash.com/3nOlkfHl3CM" alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>hogehoge@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>xxx-yyyy-zzzz</dd>
+        <dt>会社名</dt>
+        <dd>hogehoge株式会社</dd>
+        <dt>リンク</dt>
+        <dd>hogetest.com</dd>
+      </dl>
+    </div>
+  );
+};
